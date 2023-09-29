a1ec956cd08dcf3f4074a12c0d0741d632d8ca5c
vahidaa <vahidaa@code.edu.az>
2023-09-29 18:16:55 +0400

    Task D done

---
+++ TaskD/index.js
@@ -0,0 +1,29 @@
+/*
+D. Rewrite if..else using multiple ternary operators '?'. (For readability, it’s recommended to split the code into multiple lines.)
+  let message;
+  let login='Employee' 
+  if (login == 'Employee') {
+    message = 'Hello';
+  } 
+  else if (login == 'Director') {
+    message = 'Greetings';
+  } 
+  else if (login == '') {
+    message = 'No login';
+  } 
+  else {
+    message = '';
+  }
+*/
+let message;
+let login = "Employee";
+message =
+  login == "Employee"
+    ? "Hello"
+    : login == "Director"
+    ? "Greetings"
+    : login == ""
+    ? "No login"
+    : "";
+
+console.log(message);
